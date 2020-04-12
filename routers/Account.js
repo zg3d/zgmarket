8dee116ffd9e06a7ea2659e7fe16e7e5526471f7
zg3d <devzs6.98@gmail.com>
2020-04-12 12:47:09 -0400

    Update Account.js

--- routers/Account.js
+++ routers/Account.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const router = express.Router();
-const userModel = require("../models/User");
+//const userModel = require("../models/Users");
 const path = require("path");
 const bcrypt = require("bcryptjs");
 //const isAuthenticated = require("../middleware/auth");
 //const dashBoardLoader = require("../middleware/authorization");
 
 //Route to direct use to Registration form
-router.get("/sign-up",(req,res)=>
-{
-    res.render("register");
-});
+
 
 router.post("/sign-up",(req,res)=>
 { 
 
- 
+    const newUser = 
+    {
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        password:req.body.password
+    }
  
 });
 
